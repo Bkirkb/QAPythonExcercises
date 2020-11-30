692431fec79459912657bb83cd28f9921fb1d165
Bkirkb <72553105+Bkirkb@users.noreply.github.com>
2020-11-30 15:30:31 +0000

    fixed Loop excercise

--- LoopExcercise.py
+++ LoopExcercise.py
@@ -1,13 +1,14 @@
-name = str(input("What is Your name? "))
-count = 0
+def askname():
+    names = 0
+    while names < 5:
+        name = str(input("What is Your name?"))
+        print(name," Is Awesome ")
+        names += 1
 
-while count < 5:
-    print(name, " Is Awesome!")
-    count += 1
+askname()
+
+print("I hope you all have a great day!")
 
 
-for i in range(5,11):
-    print(" I is")
-
-for j in range (10,21,2):
-    print ("J is")
+for i in range (10,21,2):
+    print("This will print 6 times, because there are 6 steps of two between 10 and 21. 10, 12, 14, 16, 18 and 20).
---
+++ Timetables.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter the number to retrieve the times table"))
+count = int
+final_num = int
+
+final_num = number * number
+while number > 0 and count < final_num:
+    count += 1
+    print (number * count)
