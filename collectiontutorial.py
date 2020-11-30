a03b36e634b755efaa132c9b6bd5c30f1fd82502
Bkirkb <72553105+Bkirkb@users.noreply.github.com>
2020-11-30 11:46:09 +0000

    Collection tutorial for dictionary

---
+++ collectiontutorial.py
@@ -0,0 +1,4 @@
+books = {"Harry Potter" : "JK Rowling", "Dairy of a Wimpy Kid" : "Jeff Kinney"
+         , "Of Mice and Men" : "John Steinbeck"}
+
+print("Author is " , books["Dairy of a Wimpy Kid"])
