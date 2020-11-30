21a225925a463fba10c2edba26c912de67db8e72
Bkirkb <72553105+Bkirkb@users.noreply.github.com>
2020-11-30 14:53:14 +0000

    Loop excercise/tutorial added

---
+++ LoopExcercise.py
@@ -0,0 +1,13 @@
+name = str(input("What is Your name? "))
+count = 0
+
+while count < 5:
+    print(name, " Is Awesome!")
+    count += 1
+
+
+for i in range(5,11):
+    print(" I is")
+
+for j in range (10,21,2):
+    print ("J is")
