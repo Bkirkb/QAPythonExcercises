b5dd2ac56b527632d890c60466c2a36be6edfa30
Bkirkb <72553105+Bkirkb@users.noreply.github.com>
2020-12-01 23:02:50 +0000

    modules and importing

---
+++ excercises/divisbile_test.py
@@ -0,0 +1,7 @@
+from first_module.divisible import divisible
+
+num_one = int(input("Please enter the first number: "))
+num_two = int(input("Please enter the second number: "))
+
+print("Answer: " + str(divisible(num_one, num_two)))
+
