6e34cdf70d11c587fc3875cc6e75c19f598dafbf
Bkirkb <72553105+Bkirkb@users.noreply.github.com>
2020-12-01 23:20:26 +0000

    added dice to modules, experimented

--- excercises/divisbile_test.py
+++ excercises/divisbile_test.py
@@ -1,7 +1,10 @@
 from first_module.divisible import divisible
+from first_module.dice import dice
 
 num_one = int(input("Please enter the first number: "))
 num_two = int(input("Please enter the second number: "))
 
 print("Answer: " + str(divisible(num_one, num_two)))
+print(dice())
+print(dice())
 
